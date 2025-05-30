c11e05c4bfc8bac751fde76e51b95be913e75533
CaptainCorgi2C <webofmusic3@gmail.com>
2025-05-29 18:02:44 -0700

    The first of many

---
+++ src/main/java/net/bytemouse/short_shortcuts_api/ShortShortcutsAPIClient.java
@@ -0,0 +1,10 @@
+package net.bytemouse.short_shortcuts_api;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShortShortcutsAPIClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
