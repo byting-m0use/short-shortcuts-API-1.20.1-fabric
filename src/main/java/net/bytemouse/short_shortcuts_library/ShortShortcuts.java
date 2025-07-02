7695adb4b7d56e3d568ea87751c24d05e5398a57
ByteM0use <webofmusic3@gmail.com>
2025-07-01 17:15:44 -0700

    idk

--- src/main/java/net/bytemouse/short_shortcuts_library/ShortShortcuts.java
+++ src/main/java/net/bytemouse/short_shortcuts_library/ShortShortcuts.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ShortShortcuts implements ModInitializer {
-	public static final String MOD_ID = "short_shortcuts_api";
+	public static final String MOD_ID = "short_shortcuts_lib";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
